c7aecbe68d85e317d11e9126a2c88d637307fa90
Daniel Dyla <dyladan@gmail.com>
2017-01-30 20:15:23 -0500

    add helper method for line 2

--- BeerSong.py
+++ BeerSong.py
@@ -4,6 +4,10 @@ def main():
 def Nintey_Nine_Bottles_of_Beer():
     pass
 
-def line1(n):
+def line1():
     return '{} bottles of beer on the wall, {} bottles of beer'
 
+def line2(n):
+    if n != 1:
+        return 'take one down, pass it around, {} bottles of beer on the wall.'
+    return 'take one down, pass it around, no more bottles of beer on the wall.'
